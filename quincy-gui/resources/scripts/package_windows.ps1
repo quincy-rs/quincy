a2ee25dd1133bf6aaba718a8063de1ba773aa401
Jakub Kubík <jakub.kubik.it@protonmail.com>
2025-10-18 17:54:57 +0200

    fix: change wintun paths

--- quincy-gui/resources/scripts/package_windows.ps1
+++ quincy-gui/resources/scripts/package_windows.ps1
@@ -1,5 +1,5 @@
 cargo build --release
 
-Invoke-WebRequest https://www.wintun.net/builds/wintun-0.14.1.zip -OutFile target\tmp\wintun.zip
-Expand-Archive target\tmp\wintun.zip -DestinationPath target\tmp\
-Copy-Item target\tmp\wintun\bin\amd64\wintun.dll -Destination target\tmp\wintun.dll
+Invoke-WebRequest https://www.wintun.net/builds/wintun-0.14.1.zip -OutFile target\release\wintun.zip
+Expand-Archive target\tmp\wintun.zip -DestinationPath target\release\
+Copy-Item target\release\wintun\bin\amd64\wintun.dll -Destination target\release\wintun.dll
