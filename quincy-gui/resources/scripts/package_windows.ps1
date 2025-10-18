5b704cc09ea7bfd8505b08447ea3cb8b06c8effc
Jakub Kubík <jakub.kubik.it@protonmail.com>
2025-10-18 16:47:33 +0200

    feat: move platform-specific packager options to separate files

--- quincy-gui/resources/scripts/package_windows.ps1
+++ quincy-gui/resources/scripts/package_windows.ps1
@@ -1,3 +1,5 @@
+cargo build --release
+
 Invoke-WebRequest https://www.wintun.net/builds/wintun-0.14.1.zip -OutFile target\tmp\wintun.zip
 Expand-Archive target\tmp\wintun.zip -DestinationPath target\tmp\
 Copy-Item target\tmp\wintun\bin\amd64\wintun.dll -Destination target\tmp\wintun.dll
