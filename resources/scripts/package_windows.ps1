e3e88233c16f79a2c862a594cc763c8fc79cbe49
Jakub Kubík <jakub.kubik.it@protonmail.com>
2025-12-11 22:24:19 +0100

    fix: correct path for wintun

--- resources/scripts/package_windows.ps1
+++ resources/scripts/package_windows.ps1
@@ -1,5 +1,5 @@
 cargo build --release --bin quincy-client-gui --bin quincy-client-daemon
 
-Invoke-WebRequest https://www.wintun.net/builds/wintun-0.14.1.zip -OutFile target\tmp\wintun.zip
-Expand-Archive target\tmp\wintun.zip -DestinationPath target\tmp\
-Copy-Item target\tmp\wintun\bin\amd64\wintun.dll -Destination target\release\wintun.dll
+Invoke-WebRequest https://www.wintun.net/builds/wintun-0.14.1.zip -OutFile $env:TEMP\wintun.zip
+Expand-Archive $env:TEMP\wintun.zip -DestinationPath $env:TEMP\wintun -Force
+Copy-Item $env:TEMP\wintun\wintun\bin\amd64\wintun.dll -Destination target\release\wintun.dll
