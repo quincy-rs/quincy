7cc9e3ffe3b8ef6d181511e8eddb9bf76fe530fa
Jakub Kubík <jakub.kubik.it@protonmail.com>
2025-12-12 10:44:43 +0100

    ci: fix Linux installer build

--- resources/scripts/package_unix.sh
+++ resources/scripts/package_unix.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-cargo build --release --all-features --bin quincy-client-gui --bin quincy-client-daemon
+if [ -z "$SKIP_BUILD" ]; then
+    cargo build --release --all-features --bin quincy-client-gui --bin quincy-client-daemon
+fi
